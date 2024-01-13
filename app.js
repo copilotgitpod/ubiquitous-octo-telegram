688f62955bd6ce92a7df778a1174478260f5ac21
copilotgitpod <155841626+copilotgitpod@users.noreply.github.com>
2024-01-13 11:31:21 +0000

    a

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://flaskpi.vercel.app/api/news`
+            let url = `https://apinews24-f3abde7e8af7.herokuapp.com/api/news?api_key=53f1b64d-5042-47b5-b3d8-8f6da5f95cb3`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
-                console.log(data.news[0].tit)
+                console.log(data)
             }
             myweather()
         }
